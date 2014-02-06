5effd77469bee445ed09bf726e4c08851df5a9d0
Noel Peterson <nmpeterson@users.noreply.github.com>
2014-02-06 16:23:46 -0600

    Only copy nodes to emme_nodes_all if in tline itins

--- tmm_shp2gdb.py
+++ tmm_shp2gdb.py
@@ -19,7 +19,7 @@ import arcpy
 import TMM
 
 # Set parameters:
-shp_root_dir = arcpy.GetParameterAsText(0)  # 'C:/WorkSpace/TransitModernizationModel/TMM_Test/Media'
+shp_root_dir = arcpy.GetParameterAsText(0)  # 'C:\\WorkSpace\\TransitModernizationModel\\TMM_Test\\Media'
 
 
 # Create geodatabase:
@@ -56,49 +56,64 @@ for tod in (1, 2, 3, 4, 5, 6, 7, 8):
 
     node_fc = os.path.join(tod_fd, 'emme_nodes_{0}'.format(tod))
     tline_fc = os.path.join(tod_fd, 'emme_tlines_{0}'.format(tod))
+    tseg_fc = os.path.join(tod_fd, 'emme_tsegs_{0}'.format(tod))
 
     # Create an integer version of the nodes 'ID' field (which is FLOAT):
     arcpy.AddField_management(node_fc, TMM.node_id_field, 'LONG')
     arcpy.CalculateField_management(node_fc, TMM.node_id_field, 'int(round(!ID!))', 'PYTHON_9.3')
 
-    # Append unique nodes/tlines to all-day feature classes:
-    arcpy.AddMessage('-- Identifying unique nodes/tlines...\n')
-    day_node_fc = os.path.join(day_fd, 'emme_nodes_all')
+    # Append unique tlines to all-day fc:
+    arcpy.AddMessage('-- Identifying unique tlines...')
     day_tline_fc = os.path.join(day_fd, 'emme_tlines_all')
+    day_node_fc = os.path.join(day_fd, 'emme_nodes_all')
 
-    if not arcpy.Exists(day_node_fc):
-        arcpy.CopyFeatures_management(node_fc, day_node_fc)
-    else:
-        new_nodes_lyr = 'new_nodes_lyr'
-        new_nodes_query = ''' "{0}" NOT IN ({1}) '''.format(TMM.node_id_field, ','.join((str(node_id) for node_id in unique_nodes)))
-        arcpy.MakeFeatureLayer_management(node_fc, new_nodes_lyr, new_nodes_query)
-        arcpy.Append_management([new_nodes_lyr], day_node_fc)
-        arcpy.Delete_management(new_nodes_lyr)
-
+    new_tlines_lyr = 'new_tlines_lyr'
     if not arcpy.Exists(day_tline_fc):
-        arcpy.CopyFeatures_management(tline_fc, day_tline_fc)
+        arcpy.MakeFeatureLayer_management(tline_fc, new_tlines_lyr)
+        arcpy.CopyFeatures_management(new_tlines_lyr, day_tline_fc)
     else:
-        new_tlines_lyr = 'new_tlines_lyr'
         new_tlines_query = ''' "ID" NOT IN ('{0}') '''.format("','".join((tline_id for tline_id in unique_tlines)))
         arcpy.MakeFeatureLayer_management(tline_fc, new_tlines_lyr, new_tlines_query)
         arcpy.Append_management([new_tlines_lyr], day_tline_fc)
-        arcpy.Delete_management(new_tlines_lyr)
 
-    # Iterate through TOD FCs and identify all unique IDs:
-    unique_nodes.update((row[0] for row in arcpy.da.SearchCursor(node_fc, [TMM.node_id_field])))
-    unique_tlines.update((row[0] for row in arcpy.da.SearchCursor(tline_fc, ['ID'])))
+    # Append new unique nodes from TOD's itineraries to all-day fc:
+    arcpy.AddMessage('-- Identifying unique nodes...\n')
+    new_tlines = (row[0] for row in arcpy.da.SearchCursor(new_tlines_lyr, ['ID']))
+    new_tsegs_lyr = 'new_tsegs_lyr'
+    new_tsegs_query = ''' "LINE_ID" IN ('{0}') '''.format("','".join((tline_id for tline_id in new_tlines)))
+    arcpy.MakeFeatureLayer_management(tseg_fc, new_tsegs_lyr, new_tsegs_query)
+    new_tline_nodes = set()
+    with arcpy.da.SearchCursor(new_tsegs_lyr, ['INODE', 'JNODE']) as cursor:
+        for row in cursor:
+            new_tline_nodes.update(row)
+    new_nodes = new_tline_nodes.difference(unique_nodes)
+
+    if not arcpy.Exists(day_node_fc):
+        arcpy.CreateFeatureclass_management(os.path.split(day_node_fc)[0], os.path.split(day_node_fc)[1], 'POINT', node_fc)
+    new_nodes_lyr = 'new_nodes_lyr'
+    new_nodes_query = ''' "{0}" IN ({1}) '''.format(TMM.node_id_field, ','.join((str(node_id) for node_id in new_nodes)))
+    arcpy.MakeFeatureLayer_management(node_fc, new_nodes_lyr, new_nodes_query)
+    arcpy.Append_management([new_nodes_lyr], day_node_fc)
+
+    arcpy.Delete_management(new_tlines_lyr)
+    arcpy.Delete_management(new_tsegs_lyr)
+    arcpy.Delete_management(new_nodes_lyr)
+
+    # Update global list of unique IDs:
+    unique_tlines.update(new_tlines)
+    unique_nodes.update(new_nodes)
 
 
 # Create extra attribute tables:
-arcpy.AddMessage('Creating node extra attribute table...')
-node_table_name = 'extra_attr_nodes'
-node_table = os.path.join(TMM.gdb, node_table_name)
-arcpy.CreateTable_management(TMM.gdb, node_table_name)
-
-arcpy.AddMessage('Creating tline extra attribute table...\n')
+arcpy.AddMessage('Creating tline extra attribute table...')
 tline_table_name = 'extra_attr_tlines'
 tline_table = os.path.join(TMM.gdb, tline_table_name)
 arcpy.CreateTable_management(TMM.gdb, tline_table_name)
+
+arcpy.AddMessage('Creating node extra attribute table...\n')
+node_table_name = 'extra_attr_nodes'
+node_table = os.path.join(TMM.gdb, node_table_name)
+arcpy.CreateTable_management(TMM.gdb, node_table_name)
 
 
 # Populate extra attibute tables with unique IDs:
