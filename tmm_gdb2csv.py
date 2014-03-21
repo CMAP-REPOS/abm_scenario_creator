0b04390dd5c9dd1736b9029eef7e36b169ff27e5
Noel Peterson <nmpeterson@users.noreply.github.com>
2014-03-21 11:16:04 -0500

    Added bulk of functionality

--- tmm_gdb2csv.py
+++ tmm_gdb2csv.py
@@ -2,7 +2,7 @@
 '''
     tmm_gdb2csv.py
     Author: npeterson
-    Revised: 3/20/2014
+    Revised: 3/21/2014
     ---------------------------------------------------------------------------
     This script will use the extra attribute tables in TMM_GIS.gdb to create
     updated versions of the batchin CSVs used to construct the transit network
@@ -28,7 +28,7 @@ tod_periods = range(1, 9)  # 1-8
 tline_table = os.path.join(TMM.gdb, 'extra_attr_tlines')
 node_table = os.path.join(TMM.gdb, 'extra_attr_nodes')
 
-boarding_ease_csv_in = os.path.join(input_dir, 'boarding_ease_by_line.csv')
+boarding_ease_csv_in = os.path.join(input_dir, 'boarding_ease_by_line_id.csv')
 bus_node_attr_csv_in = os.path.join(input_dir, 'bus_node_extra_attributes.csv')
 rail_node_attr_csv_in = os.path.join(input_dir, 'rail_node_extra_attributes.csv')
 
@@ -43,25 +43,24 @@ rail_node_attr_csv_out = rail_node_attr_csv_in.replace(input_dir, output_dir)
 tline_dict = TMM.make_attribute_dict(tline_table, 'TLINE_ID', TMM.tline_fields)
 node_dict = TMM.make_attribute_dict(node_table, 'NODE_ID', TMM.node_fields)
 
-bus_csv_dict = {}
-bus_csv_fields = []
-with open(bus_node_attr_csv_in, 'r') as attr_csv:
-    dict_reader = csv.DictReader(attr_csv)
-    bus_csv_fields.extend(dict_reader.fieldnames)
-    node_id_field = bus_csv_fields[0]
-    for row_dict in dict_reader:
-        node_id = int(row_dict[node_id_field])
-        bus_csv_dict[node_id] = row_dict.copy()
+def make_dict_from_csv(csv_file_path, id_is_tline=False):
+    ''' Read a CSV and construct a dictionary whose keys are the first value in
+        each row and whose values are a dictionary of all row values stored by
+        fieldname keys. Also returns a list of CSV's fieldnames, in order. '''
+    csv_dict = {}
+    csv_fields = []
+    with open(csv_file_path, 'r') as attr_csv:
+        dict_reader = csv.DictReader(attr_csv)
+        csv_fields.extend(dict_reader.fieldnames)
+        id_field = csv_fields[0]
+        for row_dict in dict_reader:
+            dict_id = row_dict[id_field] if id_is_tline else int(row_dict[id_field])
+            csv_dict[dict_id] = row_dict.copy()
+    return csv_dict, csv_fields
 
-rail_csv_dict = {}
-rail_csv_fields = []
-with open(rail_node_attr_csv_in, 'r') as attr_csv:
-    dict_reader = csv.DictReader(attr_csv)
-    rail_csv_fields.extend(dict_reader.fieldnames)
-    node_id_field = rail_csv_fields[0]
-    for row_dict in dict_reader:
-        node_id = int(row_dict[node_id_field])
-        rail_csv_dict[node_id] = row_dict.copy()
+easeb_csv_dict, easeb_csv_fields = make_dict_from_csv(boarding_ease_csv_in, id_is_tline=True)
+bus_csv_dict, bus_csv_fields = make_dict_from_csv(bus_node_attr_csv_in)
+rail_csv_dict, rail_csv_fields = make_dict_from_csv(rail_node_attr_csv_in)
 
 
 # -----------------------------------------------------------------------------
@@ -70,41 +69,116 @@ with open(rail_node_attr_csv_in, 'r') as attr_csv:
 def adjust_type_value(node_id, csv_dict, type_field):
     ''' Create a composite score from a subset of the GDB node table's fields,
         to provide a boost to the @bstyp/@rstyp extra attributes. '''
-    max_type_value = 5  # 5 = 'major terminal'
-
-    # Set field scale factors (1 / maximum field value) & score weights
-    field_fwv = {
-        'ADD_ADA':      {'f': 1.0, 'w': 1.0},
-        'ADD_RETAIL':   {'f': 1.0, 'w': 1.0},
-        'ADD_SEATS':    {'f': 0.2, 'w': 1.0},
-        'ADD_SEC_CAM':  {'f': 0.2, 'w': 1.0},
-        'ADD_SHELTER':  {'f': 0.2, 'w': 1.0},
-        'ADD_WALKWAY':  {'f': 1.0, 'w': 1.0},
-        'ENLARGE_AREA': {'f': 0.2, 'w': 1.0},
-        'FACELIFT':     {'f': 0.2, 'w': 1.0},
-        'IMP_LIGHTING': {'f': 0.2, 'w': 1.0},
-        'IMP_WARMING':  {'f': 0.2, 'w': 1.0},
-    }
-
-    # If node is already ADA-accessible, remove 'ADD_ADA'
-    if csv_dict[node_id]['accessible'] == '1':
-        node_dict[node_id]['ADD_ADA'] = 0
-
-    # Get current field values
+    # Get current type value
     current_type_value = int(csv_dict[node_id][type_field])
-    for attr in field_fwv.keys():
-        field_fwv[attr]['v'] = int(node_dict[node_id][attr])
 
-    # Calculate node's improvement score (0-4)
-    node_improvement = sum([field_fwv[attr]['v'] * field_fwv[attr]['f'] * field_fwv[attr]['w'] for attr in field_fwv.keys()])
-    max_improvement = sum([field_fwv[attr]['w'] for attr in field_fwv.keys()])
-    pct_improvement = node_improvement / max_improvement
+    # Update type values for nodes in GDB
+    if node_id in node_dict:
+        max_type_value = 5  # 5 = 'major terminal'
 
-    # Set adjusted type value
-    max_adjustment = max_type_value - current_type_value
-    adjustment = round(max_adjustment * pct_improvement)  # The higher the current type, the harder it is to improve
-    adjusted_type_value = int(current_type_value + adjustment)
+        # Set field scale factors (1 / maximum field value) & score weights
+        field_fwv = {
+            'ADD_ADA':      {'f': 1.0, 'w': 1.0},
+            'ADD_RETAIL':   {'f': 1.0, 'w': 1.0},
+            'ADD_SEATS':    {'f': 0.2, 'w': 1.0},
+            'ADD_SEC_CAM':  {'f': 0.2, 'w': 1.0},
+            'ADD_SHELTER':  {'f': 0.2, 'w': 1.0},
+            'ADD_WALKWAY':  {'f': 1.0, 'w': 1.0},
+            'ENLARGE_AREA': {'f': 0.2, 'w': 1.0},
+            'FACELIFT':     {'f': 0.2, 'w': 1.0},
+            'IMP_LIGHTING': {'f': 0.2, 'w': 1.0},
+            'IMP_WARMING':  {'f': 0.2, 'w': 1.0},
+        }
 
-    csv_dict[node_id][type_field] = str(adjusted_type_value)
+        # If node is already ADA-accessible, remove 'ADD_ADA'
+        if csv_dict[node_id]['accessible'] == '1':
+            node_dict[node_id]['ADD_ADA'] = 0
 
-    return str(adjusted_type_value)
+        # Get current field values
+        for attr in field_fwv.keys():
+            field_fwv[attr]['v'] = int(node_dict[node_id][attr])
+
+        # Calculate node's improvement score (0-4)
+        node_improvement = sum([field_fwv[attr]['v'] * field_fwv[attr]['f'] * field_fwv[attr]['w'] for attr in field_fwv.keys()])
+        max_improvement = sum([field_fwv[attr]['w'] for attr in field_fwv.keys()])
+        pct_improvement = node_improvement / max_improvement
+
+        # Set adjusted type value
+        max_adjustment = max_type_value - current_type_value
+        adjustment = round(max_adjustment * pct_improvement)  # The higher the current type, the harder it is to improve
+        adjusted_type_value = int(current_type_value + adjustment)
+        csv_dict[node_id][type_field] = str(adjusted_type_value)
+        return str(adjusted_type_value)
+
+    # Ignore nodes not in GDB
+    else:
+        return str(current_type_value)
+
+
+def adjust_info_value(node_id, csv_dict, info_field):
+    ''' Check existing real-time info value and update if appropriate. '''
+    # Get current info value
+    current_info_value = csv_dict[node_id][info_field]
+
+    # Update info values for nodes in GDB
+    if node_id in node_dict:
+        if current_info_value == '2':
+            adjusted_info_value = current_info_value
+        else:
+            add_info = node_dict[node_id]['ADD_INFO']
+            add_pa = node_dict[node_id]['ADD_PA']
+            if add_info + add_pa > 0:
+                adjusted_info_value = '2'
+            else:
+                adjusted_info_value = current_info_value
+
+        # Set adjusted info value
+        csv_dict[node_id][info_field] = adjusted_info_value
+        adjusted_info_value = current_info_value
+        return adjusted_info_value
+
+    # Ignore nodes not in GDB
+    else:
+        return current_info_value
+
+
+# -----------------------------------------------------------------------------
+#  Update dictionary values to reflect GDB scenario.
+# -----------------------------------------------------------------------------
+for node_id in bus_csv_dict.keys():
+    adjust_type_value(node_id, bus_csv_dict, '@bstyp')
+    adjust_info_value(node_id, bus_csv_dict, '@bsinf')
+
+for node_id in rail_csv_dict.keys():
+    adjust_type_value(node_id, rail_csv_dict, '@rstyp')
+    adjust_info_value(node_id, rail_csv_dict, '@rsinf')
+
+
+# -----------------------------------------------------------------------------
+#  Write output CSVs.
+# -----------------------------------------------------------------------------
+def write_csv_from_dict(csv_file, csv_dict, csv_fields, id_is_tline=False):
+    ''' Write one of the modified CSV dictionaries and write out an updated
+        CSV file with its values. '''
+    with open(csv_file, 'wb') as attr_csv:
+        dict_writer = csv.DictWriter(attr_csv, csv_fields)
+        dict_writer.writeheader()
+        if id_is_tline:
+            def sort_tline(key):
+                ''' Order TLINE_IDs by Metra, CTA Rail, then buses. '''
+                if key.startswith('m'):         # Metra (m)
+                    return '1{0}'.format(key)
+                elif key.startswith('c'):       # CTA Rail (c)
+                    return '2{0}'.format(key)
+                else:                           # Buses (b, e, l, p, q)
+                    return '3{0}'.format(key)
+            sorted_keys = sorted(csv_dict.keys(), key=sort_tline)
+        else:
+            sorted_keys = sorted(csv_dict.keys())
+        for dict_id in sorted_keys:
+            dict_writer.writerow(csv_dict[dict_id])
+    return csv_file
+
+write_csv_from_dict(boarding_ease_csv_out, easeb_csv_dict, easeb_csv_fields, id_is_tline=True)
+write_csv_from_dict(bus_node_attr_csv_out, bus_csv_dict, bus_csv_fields)
+write_csv_from_dict(rail_node_attr_csv_out, rail_csv_dict, rail_csv_fields)
