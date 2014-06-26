85514381eb6d0f2b9fb7e8b77b65e9b78a64c2c7
Noel Peterson <nmpeterson@users.noreply.github.com>
2014-06-26 16:51:21 -0500

    No more use for R script

--- abm_trip_analysis.R
+++
@@ -1,8 +0,0 @@
-require(data.table)
-outputs_dir <- 'Y:/nmp/basic_template_20140521/model/outputs'
-setwd(outputs_dir)
-hh <- fread('hhData_1.csv')
-tours_i <- fread('indivTourData_1.csv')
-trips_i <- fread('indivTripData_1.csv')
-tours_j <- fread('jointTourData_1.csv')
-trips_j <- fread('jointTripData_1.csv')
